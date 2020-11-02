ae3d50bc70d810f954ed6b119412c69d91a9da9c
ElenaE873 <68255140+ElenaE873@users.noreply.github.com>
2020-11-02 18:48:05 -0500

    Add files via upload

---
+++ Dictionaries & SubDictionaries_andretrieval.py
@@ -0,0 +1,11 @@
+students = {
+            "Alice": {"id": "ID0001", "age":26, "gender":"F"},
+            "Bob": {"id": "ID0002", "age":27, "gender":"M"},
+            "Claire": {"id": "ID0003", "age":17, "gender":"F"},
+            "Dan": {"id": "ID0004", "age":21, "gender":"M"},
+            "Emma":{"id": "ID0005", "age":22, "gender":"F"}
+            }
+
+print(students["Dan"]["age"])
+
+print(students["Emma"]["id"], students["Emma"]["gender"])
